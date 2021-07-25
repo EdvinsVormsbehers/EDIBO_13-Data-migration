60579d8719cebacf6e853e8d0c9e3d2ecfb6efac
WhiTe <whiteedd@inbox.lv>
2021-07-25 15:54:23 +0300

    Adding code for data migrate

--- main.py
+++ main.py
@@ -18,3 +18,22 @@ music_shop_db = mysql.connector.connect(
     database="music_shop"
 )
 cursor_mysql = music_shop_db.cursor()
+
+cursor_sqlite.execute("SELECT name FROM sqlite_master WHERE type='table'")
+chinook_tables = cursor_sqlite.fetchall()
+
+cursor_mysql.execute("SET FOREIGN_KEY_CHECKS=0")
+
+for table in chinook_tables:
+    ch_table = table[0]
+    if 'sqlite' not in ch_table:
+        table_content = chinook_db.execute(f"SELECT * FROM {ch_table}")
+        columns = tuple([i[0] for i in table_content.description])
+        values = table_content.fetchall()
+
+        # migrates data from chinook database into music_shop database
+        insert = "".join((f"INSERT INTO {ch_table} {columns} VALUES {str(values).replace('[', '').replace(']', '').replace('None', 'Null')}").split("'", len(columns) * 2))
+        cursor_mysql.execute(insert)
+
+music_shop_db.commit()
+music_shop_db.close()
